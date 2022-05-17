51b4f8c1934358258ccb51e0eccc07bd4cdae792
Dylan Mayor <dylmay456@icloud.com>
2022-05-17 18:54:31 +0100

    Added RLE for line position

--- src/chunk/chunk.c
+++ src/chunk/chunk.c
@@ -1,13 +1,41 @@
 #include "chunk/chunk.h"
+#include <assert.h>
+
+static int s_prev_line = 0;
+
+static struct line_encode chunk_get_encoding(chunk_t *chunk, size_t idx)
+{
+	return *((struct line_encode *)list_get(&chunk->lines, idx));
+}
+
+struct line_encode encode_empty_lines(int begin_pos, int end_pos)
+{
+	assert(("end_pos must be greater than begin_pos", end_pos > begin_pos));
+
+	return (struct line_encode){ EMPTY_LINE, end_pos - begin_pos };
+}
 
 size_t chunk_write_code(chunk_t *chunk, code_t code, int line)
 {
-	if (!chunk->lines.cnt ||
-	    line != chunk_get_line(chunk, chunk->lines.cnt - 1)) {
-		list_write_to(&chunk->lines, 0);
+	assert(("passed line can not be zero", line != 0));
+
+	if (s_prev_line != line) {
+		struct line_encode encoding = { TOKEN_LINE, 1 };
+		struct line_encode skipped_lines =
+			encode_empty_lines(s_prev_line, line);
+
+		if (skipped_lines.count) {
+			list_write_to(&chunk->lines, &skipped_lines);
+		}
+
+		list_write_to(&chunk->lines, &encoding);
+
+		s_prev_line = line;
+	} else {
+		((struct line_encode *)list_get(&chunk->lines,
+						*(&chunk->lines.cnt) - 1))
+			->count++;
 	}
-	char *token_cnt = list_get(&chunk->lines, chunk->lines.cnt - 1);
-	token_cnt++;
 
 	return list_write_to(&chunk->code, &code);
 }
@@ -24,7 +52,24 @@ code_t chunk_get_code(chunk_t *chunk, size_t offset)
 
 int chunk_get_line(chunk_t *chunk, size_t offset)
 {
-	return *((int *)list_get(&chunk->lines, offset));
+	int cur_idx = 0;
+	int counted_code = 0;
+	int line_cnt = 0;
+
+	while (counted_code <= offset) {
+		assert(("Offset is not valid", cur_idx < &chunk->lines.cnt));
+
+		struct line_encode encoding =
+			chunk_get_encoding(chunk, cur_idx++);
+
+		if (encoding.type == TOKEN_LINE) {
+			counted_code += encoding.count;
+		} else {
+			line_cnt += encoding.count;
+		}
+	}
+
+	return line_cnt;
 }
 
 lox_val_t chunk_get_const(chunk_t *chunk, size_t offset)
--- src/chunk/chunk.h
+++ src/chunk/chunk.h
@@ -9,6 +9,13 @@ typedef enum { OP_CONSTANT, OP_RETURN } op_code_t;
 
 typedef uint8_t code_t;
 
+enum encode_type { TOKEN_LINE, EMPTY_LINE };
+
+struct line_encode {
+	enum encode_type type;
+	unsigned int count;
+};
+
 typedef struct {
 	list_t code;
 	list_t lines;
@@ -16,7 +23,8 @@ typedef struct {
 } chunk_t;
 
 #define chunk_new()                                                            \
-	((chunk_t){ list_new(code_t), list_new(int), list_new(lox_val_t) })
+	((chunk_t){ list_new(code_t), list_new(struct line_encode),            \
+		    list_new(lox_val_t) })
 
 size_t chunk_write_code(chunk_t *chunk, code_t code, int line);
 
