7f5018dbe11796ade8b0c84d47d91ab6c2bf2640
Dylan Mayor <dylmay456@icloud.com>
2022-05-19 21:48:17 +0100

    added negate op

--- src/ops/ops.c
+++ src/ops/ops.c
@@ -7,7 +7,7 @@ void op_write_const(chunk_t *chunk, lox_val_t const_val, int line)
 
 	if (const_offset <= __UINT8_MAX__) {
 		chunk_write_code(chunk, OP_CONSTANT, line);
-		chunk_write_code(chunk, const_offset, line);
+		chunk_write_code(chunk, (code_t)const_offset, line);
 	} else {
 		assert(("Max number of consts is 256 * 24",
 			const_offset < 256 * 24));
@@ -15,6 +15,11 @@ void op_write_const(chunk_t *chunk, lox_val_t const_val, int line)
 		chunk_write_code_bulk(chunk, OP_CONSTANT_LONG, line,
 				      &const_offset, 3);
 	}
+}
+
+void op_write_negate(chunk_t *chunk, int line)
+{
+	chunk_write_code(chunk, OP_NEGATE, line);
 }
 
 void op_write_return(chunk_t *chunk, int line)
--- src/ops/ops.h
+++ src/ops/ops.h
@@ -6,9 +6,11 @@
 
 #include "chunk/chunk.h"
 
-typedef enum { OP_CONSTANT, OP_CONSTANT_LONG, OP_RETURN } op_code_t;
+typedef enum { OP_CONSTANT, OP_CONSTANT_LONG, OP_NEGATE, OP_RETURN } op_code_t;
 
 void op_write_const(chunk_t *chunk, lox_val_t const_val, int line);
+
+void op_write_negate(chunk_t *chunk, int line);
 
 void op_write_return(chunk_t *chunk, int line);
 
