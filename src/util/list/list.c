cd3e09bab360f5aaaf893266eb5765683b6d7eba
Dylan Mayor <dylmay456@icloud.com>
2022-05-19 18:12:23 +0100

    List is now implemented as a stack

--- src/util/list/list.c
+++ src/util/list/list.c
@@ -7,45 +7,96 @@
 #define FREE_LIST(data_sz, pointer, old_cnt)                                   \
 	(reallocate(pointer, (data_sz)*old_cnt, 0))
 
-static void list_init(list_t *);
+static void __list_init(list_t *);
+static void __list_adj_head(list_t *lst, int cnt);
 
-size_t list_write_to(list_t *lst, void *val)
+size_t list_write_to(list_t *lst, const void *__restrict__ val)
 {
 	return list_write_bulk(lst, val, 1);
 }
 
-size_t list_write_bulk(list_t *lst, void *val, size_t cnt)
+size_t list_write_bulk(list_t *lst, const void *__restrict__ val, size_t cnt)
 {
 	size_t idx = lst->cnt;
 
 	if (lst->cap < lst->cnt + cnt) {
-		size_t old_cap = lst->cap;
-
-		lst->cap = GROW_CAPACITY_AT_LEAST(lst->cnt + cnt, old_cap);
-		lst->data =
-			GROW_LIST(lst->type_sz, lst->data, old_cap, lst->cap);
+		list_set_cap(lst,
+			     GROW_CAPACITY_AT_LEAST(lst->cnt + cnt, lst->cnt));
 	}
-	memcpy(lst->data + (lst->cnt * lst->type_sz), val, lst->type_sz * cnt);
+	memcpy(lst->head, val, lst->type_sz * cnt);
 
-	lst->cnt += cnt;
+	__list_adj_head(lst, cnt);
 
 	return idx;
+}
+
+void list_push(list_t *lst, const void *__restrict__ value)
+{
+	list_write_to(lst, value);
+}
+
+uint8_t *list_pop(list_t *lst)
+{
+	__list_adj_head(lst, -1);
+
+	return lst->head;
+}
+
+uint8_t *list_peek(list_t *lst)
+{
+	return lst->head;
+}
+
+void list_reset(list_t *lst)
+{
+	lst->cnt = 0;
+	lst->head = lst->data;
 }
 
 void list_free(list_t *lst)
 {
 	FREE_LIST(lst->type_sz, lst->data, lst->cap);
-	list_init(lst);
+	__list_init(lst);
 }
 
-char *list_get(list_t *lst, size_t idx)
+uint8_t *list_get(list_t *lst, size_t idx)
 {
 	return lst->data + (idx * (lst->type_sz));
 }
 
-static void list_init(list_t *lst)
+void list_set_cap(list_t *lst, size_t cap)
+{
+	size_t old_cap = lst->cap;
+	lst->cap = cap;
+	lst->data = GROW_LIST(lst->type_sz, lst->data, old_cap, lst->cap);
+	lst->head = lst->data + (lst->type_sz * lst->cnt);
+}
+
+void list_set_cnt(list_t *lst, size_t cnt)
+{
+	lst->cnt = cnt;
+}
+
+void list_for_each (list_t *lst, const void (*func)(void *))
+{
+	for (size_t idx = 0; idx < lst->cnt; idx++) {
+		func(list_get(lst, idx));
+	}
+}
+
+static void __list_init(list_t *lst)
 {
 	lst->cap = 0;
 	lst->cnt = 0;
 	lst->data = NULL;
+	lst->head = NULL;
 }
+
+static void __list_adj_head(list_t *lst, int cnt)
+{
+	lst->cnt += cnt;
+	lst->head += lst->type_sz * cnt;
+}
+
+#undef GROW_LIST
+#undef FREE_LIST
--- src/util/list/list.h
+++ src/util/list/list.h
@@ -8,16 +8,26 @@ typedef struct list {
 	size_t cnt;
 	size_t cap;
 	size_t type_sz;
-	char *data;
+	uint8_t *data;
+	uint8_t *head;
 } list_t;
 
-#define list_new(elem_type) ((list_t){ 0, 0, sizeof(elem_type), NULL })
+#define list_new(elem_type) ((list_t){ 0, 0, sizeof(elem_type), NULL, NULL })
 
-size_t list_write_to(list_t *lst, void *value);
+size_t list_write_to(list_t *lst, const void *__restrict__ value);
 
-size_t list_write_bulk(list_t *lst, void *value, size_t cnt);
+size_t list_write_bulk(list_t *lst, const void *__restrict__ value, size_t cnt);
 
-char *list_get(list_t *lst, size_t idx);
+uint8_t *list_get(list_t *lst, size_t idx);
+void list_set_cap(list_t *lst, size_t cap);
+void list_set_cnt(list_t *lst, size_t cnt);
 void list_free(list_t *lst);
+
+void list_push(list_t *lst, const void *__restrict__ value);
+uint8_t *list_pop(list_t *lst);
+uint8_t *list_peek(list_t *lst);
+void list_reset(list_t *lst);
+
+void list_for_each (list_t *lst, const void (*func)(void *));
 
 #endif // __CLOX_UTIL_LIST_H__
