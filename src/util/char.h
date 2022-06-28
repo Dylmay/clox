d649a6c42580f11f790145da6873bf622075b60b
Dylan Mayor <dylmay456@icloud.com>
2022-06-28 22:13:49 +0100

    fixed incorrect is_alpha check

--- src/util/char.h
+++ src/util/char.h
@@ -4,7 +4,7 @@
 #define CHAR_IS_DIGIT(chara) (chara >= '0' && chara <= '9')
 
 #define CHAR_IS_ALPHA(chara)                                                   \
-	((chara >= 'a' && chara <= 'z') || (chara == 'A' || chara == 'Z') ||   \
+	((chara >= 'a' && chara <= 'z') || (chara >= 'A' && chara <= 'Z') ||   \
 	 chara == '_')
 
 #endif // __UTIL_CHAR_H__
