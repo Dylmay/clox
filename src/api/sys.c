88d4e202a6167a5220e298cf1a0e3564869373f4
Dylan Mayor <dylmay456@icloud.com>
2022-11-11 15:40:45 +0000

    Added num() and round() funcs

--- src/api/sys.c
+++ src/api/sys.c
@@ -1,7 +1,9 @@
 #include "sys.h"
+#include "util/dtoa.h"
 
 #include <time.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "val/func/val_func.h"
 #include "val/func/object_func.h"
@@ -10,8 +12,11 @@
 static lox_val_t __clock_native(int arg_cnt, lox_val_t *args);
 static lox_val_t __print_native(int arg_cnt, lox_val_t *args);
 static lox_val_t __to_str(int arg_cnt, lox_val_t *args);
+static lox_val_t __to_num(int arg_cnt, lox_val_t *args);
 static lox_val_t __assert(int arg_cnt, lox_val_t *args);
 static lox_val_t __read(int arg_cnt, lox_val_t *args);
+static lox_val_t __round_val(int arg_cnt, lox_val_t *args);
+static lox_val_t __round_double(double value, int round_to);
 
 #define CREATE_FUNC_DEF(func_name, func_def)                                   \
 	{                                                                      \
@@ -23,14 +28,17 @@ static struct native_import imports[] = {
 	CREATE_FUNC_DEF("clock", __clock_native),
 	CREATE_FUNC_DEF("print", __print_native),
 	CREATE_FUNC_DEF("str", __to_str),
+	CREATE_FUNC_DEF("num", __to_num),
 	CREATE_FUNC_DEF("assert", __assert),
 	CREATE_FUNC_DEF("read", __read),
+	CREATE_FUNC_DEF("round", __round_val),
 };
 
 #undef CREATE_FUNC_DEF
 
 #define FILE_READ_ERR_MSG "Read Error"
 #define ASSERT_ERR_MSG "Assert Error"
+#define NAN_ERR_MSG "Argument is not a number"
 
 static struct import_list import_list = {
 	.import_name = "sys",
@@ -119,4 +127,108 @@ static lox_val_t __read(int arg_cnt, lox_val_t *args)
 	string_free(read_input);
 
 	return str_obj;
+}
+
+static lox_val_t __round_val(int arg_cnt, lox_val_t *args)
+{
+	if (arg_cnt) {
+		lox_val_t val_to_round = args[0];
+		int round_amt = 0;
+
+		if (!VAL_IS_NUMBER(val_to_round)) {
+			return VAL_CREATE_ERR(object_str_new(
+				NAN_ERR_MSG, sizeof(NAN_ERR_MSG) - 1));
+		}
+
+		if (arg_cnt > 1) {
+			lox_val_t round_val = args[1];
+
+			if (!VAL_IS_NUMBER(round_val)) {
+				return VAL_CREATE_ERR(object_str_new(
+					NAN_ERR_MSG, sizeof(NAN_ERR_MSG) - 1));
+			}
+
+			double rounded_val = floor(VAL_AS_NUMBER(round_val));
+
+			if (rounded_val < 0) {
+				return VAL_CREATE_ERR(object_str_new(
+					"round amount cannot be negative",
+					sizeof("round amount cannot be negative") -
+						1));
+			}
+
+			round_amt = rounded_val;
+		}
+
+		if (round_amt == 0) {
+			return VAL_CREATE_NUMBER(
+				round(VAL_AS_NUMBER(val_to_round)));
+		}
+
+		return __round_double(VAL_AS_NUMBER(val_to_round), round_amt);
+	}
+
+	return VAL_CREATE_ERR(
+		object_str_new("round() requires a number",
+			       sizeof("round() requires a number") - 1));
+}
+
+static lox_val_t __round_double(double value, int round_to)
+{
+#define SMALL_BUF_LEN 100
+	char *buf, *buf_end;
+	char small_buf[SMALL_BUF_LEN];
+	size_t temp_buf_sz = SMALL_BUF_LEN;
+	char *temp_buf = small_buf;
+	int sign, decpt;
+
+	buf = dtoa(value, 3, round_to, &decpt, &sign, &buf_end);
+
+	if ((buf_end - buf) + 9 > SMALL_BUF_LEN) {
+		temp_buf_sz = (buf_end - buf) + 8;
+		temp_buf = reallocate(NULL, 0, temp_buf_sz);
+
+		// TODO: check for alloc issues
+	}
+
+	snprintf(temp_buf, temp_buf_sz, "%s0%se%d", (sign ? "-" : ""), buf,
+		 decpt - (int)(buf_end - buf));
+
+	lox_val_t val = VAL_CREATE_NUMBER(strtod(temp_buf, NULL));
+
+	if (temp_buf != small_buf) {
+		reallocate(temp_buf, temp_buf_sz, 0);
+	}
+	freedtoa(buf);
+
+	return val;
+#undef SMALL_BUF_LEN
+}
+
+static lox_val_t __to_num(int arg_cnt, lox_val_t *args)
+{
+	if (arg_cnt) {
+		lox_val_t arg = args[0];
+
+		if (VAL_IS_BOOL(arg)) {
+			return VAL_CREATE_NUMBER(VAL_AS_BOOL(arg) ? 1 : 0);
+		}
+
+		if (VAL_IS_NUMBER(arg)) {
+			return arg;
+		}
+
+		if (OBJECT_IS_STRING(arg)) {
+			const lox_str_t *string = OBJECT_AS_STRING(arg);
+
+			if (string->len > 0) {
+				return VAL_CREATE_NUMBER(
+					strtod(string->chars, NULL));
+			}
+		}
+	}
+
+	return VAL_CREATE_ERR(
+		object_str_new("invalid literal for num()",
+			       sizeof("invalid literal for num()") - 1));
 }
